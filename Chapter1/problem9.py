f5644e336504ccc40dfa63957f75963fd1e82a13
Ivy <mhichen@umich.edu>
2014-03-17 23:29:01 -0700

    Finished problem 9

---
+++ Chapter1/problem9.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+#********************************************************
+# Given sorted array A and an integer S, returns true
+# if there are a pair of indices, i and j, such that
+# A[i] + A[j] = S (where i and j aren't necessarily
+# distinct)
+#********************************************************
+
+def contains_sum_equal_to(S, array):
+
+    # Make a dictionary out of the array
+    new_list = set(array)
+
+    # Loop over the dictionary
+    # If
+    for i in new_list:
+        if ( (S - i) in new_list ):
+            return True
+
+    return False
+
+
+
+myarray = [0, 2, 4, 5, 10, 12, 16, 19]
+
+print(contains_sum_equal_to(11, myarray))
