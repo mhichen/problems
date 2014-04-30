53398fa546959304a9e1c476099a11fb378a095c
Ivy <mhichen@umich.edu>
2014-04-29 22:50:30 -0700

    Wrote a routine that uses a bitwise operation to find the lone, unpaired number in a list of paired integers.

--- Miscellaneous/find_unpaired.py
+++ Miscellaneous/find_unpaired.py
@@ -5,3 +5,20 @@
 # except for one, find the unpaired one
 #********************************************
 
+def find_unpaired(numbers):
+    
+    sum = 0
+    
+    for i in numbers:
+        
+        sum ^= i
+        
+    return sum
+
+# Test function
+if __name__ == "__main__":
+    
+    list_of_ints = [0, 4, 2, 0, 3, 2, 4]
+    
+    print(find_unpaired(list_of_ints))
+    
