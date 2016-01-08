a3b96144ae75c50319b0c5e0fbb658de715d478d
Min-Hua Ivy Chen <mhichen@umich.edu>
2016-01-07 18:17:29 -0600

    Wrote reverseinteger.cpp

--- QFinance_Interviews/LeetCode/reverseinteger.cpp
+++ QFinance_Interviews/LeetCode/reverseinteger.cpp
@@ -11,38 +11,19 @@ public:
   int ReverseInteger(int num)
   {
 
-    int result;
-    
-    // Get sign of number
-    int sign = 1;
-    if (num < 0)
-    {
-      sign = -1;
-      num = -num;
-    }
-    
-    std::deque<int> tdeq;
+    int result = 0;
 
-    while(num > 0)
-    {
-      tdeq.push_back(num%10);
-      num = num/10;
-      std::cout << num << "\n";
+    while (num != 0){
+      if (num > 214748364){
+	return result;
+      }
+
+      result = result * 10 + num%10;
+
+      num /= 10;
+      
     }
 
-    std::cout << "\n";
-    
-    result = tdeq.front();
-    tdeq.pop_front();
-    
-    while (!tdeq.empty())
-    {
-      result = result * 10 + tdeq.front();
-      std::cout << result << "\n";
-      tdeq.pop_front();
-    }
-    
-    result *= sign;
     return result;
   }
 
@@ -53,7 +34,7 @@ public:
 int main()
 {
 
-  int number = -100;
+  int number = 123;
 
   Solution sol;
   
