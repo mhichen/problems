b04d172ddb3cc7f87a25856ab971c691a74c8fa5
Min-Hua Ivy Chen <mhichen@umich.edu>
2016-06-09 16:22:06 -0500

    Re-did Reverse Words in String.

--- QFinance_Interviews/LeetCode/reversestring.cpp
+++ QFinance_Interviews/LeetCode/reversestring.cpp
@@ -13,34 +13,47 @@ public:
    */
   std::string reverse_string(std::string input)
   {
+
     std::string reversed;
 
-    // j keeps track of index of end of word
+    // Start at end of the input string and index backwards
+    // When the beginning of a word is reached, then the word
+    // is stored into the output string
+    
+    // Index keeping track of end of word
     int j = input.length();
 
-    // i keeps track of index of beginning of word
-    for (int i = input.length() - 1; i >= 0; i--)
+    // Index keeping track of beginning of word
+    for (int i = input.length() - 1; i>= 0; i--)
     {
-      // if trailing spaces, then reset j index
+
+      // check for trailing spaces
       if (input[i] == ' ')
       {
-	j = i;
-      } // otherwise, if the character preceding i is a space
-      // then we have found the start of a word, so we add a space
-      // to the reversed string, then add the word itself
+	j = i; // reset the end of word to be where index i is
+      }
+      // otherwise, either already reached the very beginning of the
+      // string OR at the beginning of a word (verified by checking to
+      // see if the previous (i-1) char is a space)
       else if (i == 0 || input[i-1] == ' ')
       {
+	// Add a space if there have already been words added to the
+	// result string
 	if (reversed.length() != 0)
 	{
 	  reversed.append(" ");
 	}
+	// tack on the word
 	reversed.append(input.substr(i, j-i));
       }
+
+      // If the index i is in the middle of a word, everything
+      // in this block is simply skipped and
       
     }
-    
 
     return reversed;
+
   }
   
   
